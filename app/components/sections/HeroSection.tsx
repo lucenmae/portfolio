200966e37ab270816317b708512882fd14990433
lucenmae <lucymaetan@gmail.com>
2025-02-19 23:18:59 +0800

    refactor: Simplify HeroSection text animation

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -6,10 +6,6 @@ import SplitText from "@/app/components/text-animations/SplitText";
 import { easings } from "@react-spring/web";
 
 const HeroSection: FC = () => {
-  const handleAnimationComplete = () => {
-    // You can add any logic here that should run after the animation completes
-    console.log("Animation completed!");
-  };
 
   return (
     <section className="relative w-full h-screen">
@@ -27,7 +23,7 @@ const HeroSection: FC = () => {
       {/* Split Text Container */}
       <div className="relative z-10 flex items-center justify-center w-full h-full">
         <SplitText
-          text="Hello! I'm Lucy :)"
+          text="Hi! I'm Lucy ^-^"
           className="text-white text-6xl font-bold"
           delay={130}
           animationFrom={{ opacity: 0, transform: "translate3d(0,50px,0)" }}
@@ -35,7 +31,6 @@ const HeroSection: FC = () => {
           easing={easings.easeOutCubic}
           threshold={0.2}
           rootMargin="-50px"
-          onLetterAnimationComplete={handleAnimationComplete}
         />
       </div>
     </section>
