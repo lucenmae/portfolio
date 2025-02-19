c870295d16b31878bca7ba5988ca9088e4469e8e
lucenmae <lucymaetan@gmail.com>
2025-02-19 22:18:08 +0800

    feat: Add initial site configuration with navigation and links

---
+++ config/site.ts
@@ -0,0 +1,23 @@
+export const siteConfig = {
+    name: "Lucy Mae Tan",
+    title: "Lucy Mae Tan | Portfolio",
+    description: "Personal portfolio website. Everything I've done when I am bored, inspired, or just want to share.",
+    mainNav: [
+      {
+        title: "Home",
+        href: "/",
+      },
+      {
+        title: "About",
+        href: "/about",
+      },
+      {
+        title: "Projects",
+        href: "/projects",
+      }
+    ],
+    links: {
+      github: "https://github.com/lucenmae",
+      linkedin: "https://linkedin.com/in/lucymaetan",
+    },
+  }
