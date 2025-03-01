8b43010cc9e446606e757e5a6cd0104717924a5e
lucenmae <lucymaetan@gmail.com>
2025-03-01 23:10:09 +0800

    feat: Add ProjectsSection to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/app/components/sections/HeroSection"
+import ProjectsSection from "@/app/components/sections/ProjectsSection"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#060606]">
       <HeroSection />
+      <ProjectsSection />
     </main>
   )
 }
